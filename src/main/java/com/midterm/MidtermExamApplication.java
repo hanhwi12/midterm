5635d74b4bd224b827f78bf381d6595859e61360
hiphop9n <hiphop9n@gmail.com>
2025-04-18 14:06:27 +0900

    first commit

---
+++ src/main/java/com/midterm/MidtermExamApplication.java
@@ -0,0 +1,14 @@
+package com.midterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidtermExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidtermExamApplication.class, args);
+		System.out.println("Midterm Exam Application Started");
+	}
+
+}
